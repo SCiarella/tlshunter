bc8ded6667358580618702bfdfd10e7464110529
SCiarella <simoneciarella@gmail.com>
2024-11-16 14:52:59 +0100

    Create package

---
+++ src/tlshunter/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about tlshunter."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Simone Ciarella"
+__email__ = "simoneciarella@gmail.com"
+__version__ = "0.1.0"
